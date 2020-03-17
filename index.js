b21fab05df2f742d26403d7b9ebabe7fc627c699
ciamac <hgzguzg>
2020-03-17 16:19:47 +0100

    Verified by Cia v1.0.2

--- index.js
+++ index.js
@@ -23,6 +23,22 @@ app.get('/', (req, res) => {
     return res.sendFile(__dirname + '/index.html');
 });
 
+  // Guthentictation für die Kanal;
+  app.post("/pusher/auth", (req, res) => {
+      const socketId = req.body.socket_id;
+      const channel = req.body.channel_name;
+      var presenceData = {
+        user_id:
+          Math.random()
+            .toString(36)
+            .slice(2) + Date.now()
+      };
+      const auth = pusher.authenticate(socketId, channel, presenceData);
+      res.send(auth);
+    });
+
+
+
 //listen Port
 app.listen(3000, () => {
     return console.log('Server is up on 3000')
